b73504382f9d639dbc1fe32cdf2f75b45bff9019
yanchao <yanchao@hiynn.com>
2017-12-01 18:54:09 +0800

    '设置跨域无效'

---
+++ cas-permissions-server/src/main/java/com/hiynn/caspermissions/server/web/controller/NoProtectedController.java
@@ -0,0 +1,17 @@
+package com.hiynn.caspermissions.server.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author yanchao
+ * @date 2017/12/1 14:21
+ */
+@RestController
+public class NoProtectedController {
+
+    @GetMapping(value = "/hello1")
+    public String test() {
+        return "Hello World!";
+    }
+}
