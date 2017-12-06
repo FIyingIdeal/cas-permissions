acbe2e00d5a61abb7b1d0b7244b4006d8f922f1b
yanchao <yanchao@hiynn.com>
2017-12-06 15:33:44 +0800

    修改配置

--- cas-permissions-server/src/main/java/com/hiynn/caspermissions/server/CasPermissionsServerApplication.java
+++ cas-permissions-server/src/main/java/com/hiynn/caspermissions/server/CasPermissionsServerApplication.java
@@ -2,8 +2,6 @@ package com.hiynn.caspermissions.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 
 @SpringBootApplication
 public class CasPermissionsServerApplication {
